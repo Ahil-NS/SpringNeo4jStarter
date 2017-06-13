ba83ea95e06be7ccab0d6ba35ac5dd23dcc109d1
Ahil <Ahil@DESKTOP-L00D2SK>
2017-06-13 12:55:54 +0530

    Add domain

---
+++ src/main/java/com/neo4j/spring/domain/User.java
@@ -0,0 +1,30 @@
+package com.neo4j.spring.domain;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity
+public class User {
+
+	@GraphId
+	private String name;
+
+	public User() {
+
+	}
+
+	public User(String name) {
+
+		this.name = name;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
