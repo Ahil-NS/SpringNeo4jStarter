14a3ca71cf5a31329d15970a0a68f0c732544fa8
Ahil <Ahil@DESKTOP-L00D2SK>
2017-06-13 13:34:05 +0530

    UserController basic

--- src/main/java/com/neo4j/spring/controller/UserController.java
+++ src/main/java/com/neo4j/spring/controller/UserController.java
@@ -1,5 +1,21 @@
 package com.neo4j.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+	
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST, produces = "application/json")
+	public void createUser(@RequestParam String name) {
+		System.out.println(name);
+	
+	}
+	
 
 }
+
